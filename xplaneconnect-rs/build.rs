857b4d648f0c03a3d77219c8b95214b4e4978f7e
Ryan Webber <wbbr.ryan@gmail.com>
2024-08-26 14:44:05 -0700

    Include xplaneconnect licence documents with vendored source

--- xplaneconnect-rs/build.rs
+++ xplaneconnect-rs/build.rs
@@ -1,11 +1,11 @@
 use cc;
 
 fn main() {
-    println!("cargo:rerun-if-changed=src/xplaneconnect.c");
-    println!("cargo:rerun-if-changed=src/xplaneconnect.h");
+    println!("cargo:rerun-if-changed=src/XPlaneConnect/xplaneconnect.c");
+    println!("cargo:rerun-if-changed=src/XPlaneConnect/xplaneconnect.h");
 
     cc::Build::new()
-        .file("src/xplaneconnect.c")
+        .file("src/XPlaneConnect/xplaneconnect.c")
         .flag("-Wno-pointer-sign")
         .flag("-Wno-unused-but-set-parameter")
         .flag("-Wno-tautological-constant-out-of-range-compare")
